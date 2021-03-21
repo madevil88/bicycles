d37111930a9f8554a0accbf05aa244509bed13d4
madevil88 <madevil@i.ua>
2021-03-22 00:34:55 +0200

    Финиш (без полной проверки)

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
 (function () {
+  const navMain = document.querySelector(`.page-header__main-nav`);
+  const navToggle = document.querySelector('.page-header__main-nav-toggle');
 
+  if (navMain && navToggle) {
+    navMain.classList.remove('page-header__main-nav--nojs')
+    navMain.classList.add('page-header__main-nav--closed');
+
+    navToggle.addEventListener('click', function() {
+      navMain.classList.toggle('page-header__main-nav--closed')
+    });
+  }
 })();
