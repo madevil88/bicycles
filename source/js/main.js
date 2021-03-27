38a8c21f20ba4ab84879e1d69db6cb80a8514ad1
madevil88 <madevil@i.ua>
2021-03-27 18:39:21 +0200

    Исправлены все 18 багов

--- source/js/main.js
+++ source/js/main.js
@@ -3,13 +3,20 @@
 (function () {
   const navMain = document.querySelector('.page-header__main-nav');
   const navToggle = document.querySelector('.page-header__main-nav-toggle');
+  const menuItems = document.querySelectorAll('.page-header__site-menu-item');
 
   if (navMain && navToggle) {
     navMain.classList.remove('page-header__main-nav--nojs')
     navMain.classList.add('page-header__main-nav--closed');
 
     navToggle.addEventListener('click', function() {
-      navMain.classList.toggle('page-header__main-nav--closed')
+      navMain.classList.toggle('page-header__main-nav--closed');
+    });
+
+    menuItems.forEach((menuItem) => {
+      menuItem.addEventListener('click', function() {
+        navMain.classList.toggle('page-header__main-nav--closed');
+      });
     });
   }
 })();
