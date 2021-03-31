0742569e95c2ce4624a97dd44b0adb59d304fcdc
madevil88 <madevil@i.ua>
2021-03-31 14:37:46 +0300

    Исправлено 5 багов

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,29 @@
 'use strict';
 
 (function () {
-  const navMain = document.querySelector('.page-header__main-nav');
-  const navToggle = document.querySelector('.page-header__main-nav-toggle');
-  const menuItems = document.querySelectorAll('.page-header__site-menu-item');
+  const pageBody = document.querySelector('body');
+  const navMain = pageBody.querySelector('.page-header__main-nav');
+  const navToggle = pageBody.querySelector('.page-header__main-nav-toggle');
+  const menuItems = pageBody.querySelectorAll('.page-header__site-menu-item');
+
+  const showMenu = () => {
+    if (navMain.classList.contains('page-header__main-nav--closed')) {
+        navMain.classList.remove('page-header__main-nav--closed');
+        pageBody.style.overflow = 'hidden';
+      } else {
+        navMain.classList.add('page-header__main-nav--closed');
+        pageBody.style.overflow = 'scroll';
+      }
+  };
 
   if (navMain && navToggle) {
     navMain.classList.remove('page-header__main-nav--nojs')
     navMain.classList.add('page-header__main-nav--closed');
 
-    navToggle.addEventListener('click', function() {
-      navMain.classList.toggle('page-header__main-nav--closed');
-    });
+    navToggle.addEventListener('click', showMenu);
 
     menuItems.forEach((menuItem) => {
-      menuItem.addEventListener('click', function() {
-        navMain.classList.toggle('page-header__main-nav--closed');
-      });
+      menuItem.addEventListener('click', showMenu);
     });
   }
 })();
